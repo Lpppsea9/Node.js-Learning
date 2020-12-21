777adc9bdab7d1ae0d69865bdde86a71b7a7bc36
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-21 12:38:22 +0800

    12/21

---
+++ commonjs.test/b.js
@@ -0,0 +1,15 @@
+const { conformsTo } = require("lodash");
+const {add,mul,a} = require("./a")
+const _ = require('lodash')
+
+
+const sum = add(10,12)
+const res = mul(100,200)
+console.log(sum);
+console.log(res);
+// console.log(a);
+
+const arr = _.concat([1,2],3)
+console.log("arr =",arr);
+
+
