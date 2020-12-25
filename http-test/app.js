9aab69fb9ff63cf593bce32d937f61b507233e5f
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-25 17:32:31 +0800

    12/25

--- http-test/app.js
+++ http-test/app.js
@@ -1,15 +1,39 @@
 const http = require('http')
 const querystring = require('querystring')
 
-const server = http.createServer((req,res) => {
-  console.log('method:', req.method); //GET
+const server = http.createServer((req, res) => {
+  const method = req.method
   const url = req.url
-  console.log('url:', url);
-  req.query = querystring.parse(url.split('?')[1])
-  console.log('query:', req.query)
-  res.end(
-    JSON.stringify(req.query)
-  )
+  const path = url.split('?')[0]
+  const query = querystring.parse(url.split('?')[1])
+
+  // 设置返回格式为 JSON, text/html说明 res.end 返回的是html
+  res.setHeader('Content-type', 'application/json') 
+
+  // 返回的数据
+  const resData = {
+    method,
+    url,
+    path,
+    query
+  }
+
+  //返回
+  if(method === 'GET') {
+    res.end(
+      JSON.stringify(resData)
+    )
+  }
+  if(method === 'POST') {
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      resData.postData = postData
+      //  返回
+    })
+  }
 })
 
 server.listen(8000)
@@ -18,5 +42,6 @@ console.log('OK');
 
 /* 
   通过GET获取数据    
+  通过POST传输数据
 */
 
