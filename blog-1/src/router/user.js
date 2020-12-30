4c9dde68911e6b5a928d8ee34cfc315846b2fad9
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-30 17:55:30 +0800

    12/30

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,4 +1,6 @@
-const { login } = require('../controller/user')
+const { loginCheck } = require('../controller/user')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
 
 const handleUserRouter = (req, res) => {
   const method = req.method
@@ -6,6 +8,11 @@ const handleUserRouter = (req, res) => {
   // 登录
   if(method === 'POST' && req.path === '/api/user/login') {
     const { username, password } = req.body
+    const result = loginCheck(username, password)
+    if (result) {
+        return new SuccessModel()
+    }
+    return new ErrorModel('登陆失败')
   }
 }
 
